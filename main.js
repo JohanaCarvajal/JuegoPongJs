abbb78011484f1fa5748a2d8d31453afd4c7d8e1
Johana Carvajal <johanadev94@gmail.com>
2021-09-08 15:15:20 -0500

    Se brinda movimiento a las barras

--- main.js
+++ main.js
@@ -23,6 +23,7 @@
 
 })(); 
 
+
 (function(){
     self.Bar = function (x,y,width,height,board){
         this.x = x;
@@ -62,48 +63,79 @@
     }
 
     self.BoardView.prototype = {
+        //borra fillRect, lo va reiniciando
+        clean: function(){
+            this.ctx.clearRect(0,0,this.board.witdh,this.board.height) 
+        },
+
         draw: function (){
             for (var i = this.board.elements.length -1; i >=0;i--){
                 var el = this.board.elements[i];
 
                 draw(this.ctx,el);
-            }
+            };
+        },
+        play: function (){
+            this.clean();
+            this.draw();
         }
     }
 
     function draw (ctx,element){
-        //hasOwnProperty Me dice si el objeto tiene una propiedad kind
-        if (element !== null && element.hasOwnProperty ("kind")) {
+        //hasOwnProperty Me dice si el objeto tiene una propiedad kind       
             switch(element.kind){
                 case "rectangle":
                     ctx.fillRect (element.x,element.y,element.width,element.height);
                     break;
-            }
+                case "circle":
+                    ctx.beginPath();
+                    ctx.arc(element.x,element.y, element.radius,0,7);    
+                    ctx.fill();
+                    ctx.closePath();
+                    break;
             
-        }
-        
+            
+            
+                }       
+
+
+             
+             
     }
 
 })();
+
+var board = new Board (800,400);   
+var bar = new Bar (20,100,40,100,board);
+var bar_2 = new Bar (700,100,40,100,board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas,board);
+var ball =  new Ball (350,100,10,board);
+
+
+
 document.addEventListener ("keydown",function (ev){
+    ev.preventDefault();
     
-    if (ev.keyCode == 38) {
+    if (ev.KeyCode == 38) {
         bar.up ();
     }
-    else if (ev.keyCode== 40 ) {
+    else if (ev.KeyCode== 40 ) {
         bar.down ();
+    }else if (ev.KeyCode === 87) {
+        //w
+        bar_2.up();
+    }else if (ev.KeyCode === 83) {
+        //S
+        bar_2.down();
     }
-    console.log(""+bar);
+    console.log(""+bar_2);
 });
-window.addEventListener("load",main);
-
+//window.addEventListener("load",main);
 //funcion que va a ejecutar todos los elementos
-function main (){
-    var board = new Board (800,400);   
-    var bar = new Bar (20,100,40,100,board);
-    var bar = new Bar (700,100,40,100,board);
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas,board);
-    console.log(board); 
-    board_view.draw();
+
+window.requestAnimationFrame(controller);
+function controller (){
+    board_view.play();    
+    window.requestAnimationFrame(controller);
 }
