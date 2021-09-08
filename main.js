5791eea14d9c9dad9e045a904d2c1e0b768f14c5
Johana Carvajal <johanadev94@gmail.com>
2021-09-08 15:59:40 -0500

    Se configura el movimiento a la bola de Pong

--- main.js
+++ main.js
@@ -9,13 +9,14 @@
         this.game_over = false;
         this.bars = [];
         this.ball = null;
+        this.playing = false;
     }
 //modificar prototipo de la clase para colocar los metods  de la misma clase
 
     // metodo retorna barras y tableros
     self.Board.prototype = {
         get elements (){
-            var elements = this.bars// Barras del juego que controlan al usuario
+            var elements = this.bars.map(function(bar){return bar;});// Barras del juego que controlan al usuario
             elements.push (this.ball);
             return elements;
         }   
@@ -30,8 +31,17 @@
         this.speed_y=0;
         this.speed_x=3;
         this.board = board;
+        this.direction = 1 ;
+
         board.ball = this;
-        this.kind = "circle";
+        this.kind = "circle";       
+    }
+
+    self.Ball.prototype = {
+        move: function (){
+            this.x += (this.speed_x * this.direction);
+            this.y += (this.speed_y);
+        }
     }
 })();
 
@@ -87,8 +97,12 @@
             };
         },
         play: function (){
-            this.clean();
+            if (this.board.playing){
+                this.clean();
             this.draw();
+            this.board.ball.move();
+            }
+            
         }
     }
 
@@ -126,26 +140,37 @@ var ball =  new Ball (350,100,10,board);
 
 
 document.addEventListener ("keydown",function (ev){
-    ev.preventDefault();
+    
     
     if (ev.KeyCode == 38) {
+        ev.preventDefault();
         bar.up ();
     }
     else if (ev.KeyCode== 40 ) {
+        ev.preventDefault();
         bar.down ();
     }else if (ev.KeyCode === 87) {
+        ev.preventDefault();
         //w
         bar_2.up();
     }else if (ev.KeyCode === 83) {
+        ev.preventDefault();
         //S
         bar_2.down();
+    }else if (ev.keyCode === 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
     console.log(""+bar_2);
 });
 //window.addEventListener("load",main);
 //funcion que va a ejecutar todos los elementos
 
+board_view.draw();
 window.requestAnimationFrame(controller);
+setTimeout (function (){
+    ball.direction = -1;
+},4000);
 function controller (){
     board_view.play();    
     window.requestAnimationFrame(controller);
