776ee593563178f4304fce7ea2646d00ede63503
Johana Carvajal <johanadev94@gmail.com>
2021-09-07 23:42:57 -0500

    Se crean funciones principales mvc para mostrar tablero y barras

---
+++ main.js
@@ -0,0 +1,96 @@
+
+ //crear funcion anónima
+(function(){
+    //Creando clase, recibe por param largo y ancho del tablero
+    self.Board = function (width,height){
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+    }
+//modificar prototipo de la clase para colocar los metods  de la misma clase
+
+    // metodo retorna barras y tableros
+    self.Board.prototype = {
+        get elements (){
+            var elements = this.bars// Barras del juego que controlan al usuario
+            elements.push (this.ball);
+            return elements;
+        }   
+    }
+
+})(); 
+
+(function(){
+    self.Bar = function (x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        //  Ingreso al board, luego al arreglo bars y le adiciono con push todo este objeto, esta barra
+        this.board.bars.push(this);
+        //dibujar las cosas
+        this.kind = "rectangle";  
+        
+    }
+    //moverlo
+    self.Bar .prototype = {
+        down: function (){
+
+        },
+        up: function(){
+
+        }
+    }
+
+})();
+
+(function(){
+    self.BoardView =function(canvas,board){
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+
+    self.BoardView.prototype = {
+        draw: function (){
+            for (var i = this.board.elements.length -1; i >=0;i--){
+                var el = this.board.elements[i];
+
+                draw(this.ctx,el);
+            }
+        }
+    }
+
+    function draw (ctx,element){
+        //hasOwnProperty Me dice si el objeto tiene una propiedad kind
+        if (element !== null && element.hasOwnProperty ("kind")) {
+            switch(element.kind){
+                case "rectangle":
+                    ctx.fillRect (element.x,element.y,element.width,element.height);
+                    break;
+            }
+            
+        }
+        
+    }
+
+})();
+
+window.addEventListener("load",main);
+
+//funcion que va a ejecutar todos los elementos
+function main (){
+    var board = new Board (800,400);   
+    var bar = new Bar (20,100,40,100,board);
+    var bar = new Bar (700,100,40,100,board);
+    var canvas = document.getElementById('canvas');
+    var board_view = new BoardView(canvas,board);
+    console.log(board); 
+    board_view.draw();
+}
